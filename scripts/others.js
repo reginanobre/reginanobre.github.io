d07d5ca28cc9fa1dc624f61d25d5b3e2238750c1
reginanobre <47428345+reginanobre@users.noreply.github.com>
2021-07-21 18:38:49 +0200

    Update others.js

--- scripts/others.js
+++ scripts/others.js
@@ -1,12 +1,12 @@
 var myothers = '<h2>In press</h2> \
 <ul> \
   <li> \
-  <a href="https://artsetmetiers.fr/fr/actualites/lintelligence-artificielle-vue-par-joris-guerin-doctorant-sur-le-campus-arts-et-metiers"> \
-  L’intelligence artificielle vue par Joris Guerin, doctorant sur le campus Arts et Métiers de Lille</a> \
+  <a href=" http://www.diretodaciencia.com/2020/03/04/protecao-do-codigo-florestal-para-lagos-e-represas-e-insuficiente-no-semiarido"> \
+  Forest Code Protection for lakes and dams is insufficient in the Semiarid Region – National press release</a> \
   </li> \
   <li> \
-  <a href="https://artsetmetiers.fr/fr/actualites/de-lille-atlanta-le-doctorat-de-joris-guerin"> \
-  De Lille à Atlanta: Le doctorat de Joris Guérin</a> \
+  <a href="https://abori.com.br/ambiente/area-de-conservacao-de-entorno-de-lagos-e-represas-precisa-ser-maior-do-que-diz-codigo-florestal/"> \
+  Conservation area around lakes and dams needs to be larger than what Forest Code states – National press release </a> \
   </li> \
 </ul> \
 <br>'
@@ -17,3 +17,4 @@ $(document).ready(function(){
     $(".content").append(myothers);
   });
 });
+
