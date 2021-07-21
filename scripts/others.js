50d680732cced66feabb34e09955346111a001cb
reginanobre <47428345+reginanobre@users.noreply.github.com>
2021-07-21 18:40:27 +0200

    Update others.js

--- scripts/others.js
+++ scripts/others.js
@@ -1,11 +1,11 @@
 var myothers = '<h2>In press</h2> \
 <ul> \
   <li> \
-  <a href=" http://www.diretodaciencia.com/2020/03/04/protecao-do-codigo-florestal-para-lagos-e-represas-e-insuficiente-no-semiarido"> \
+  <a href="http://www.diretodaciencia.com/2020/03/04/protecao-do-codigo-florestal-para-lagos-e-represas-e-insuficiente-no-semiarido">\
   Forest Code Protection for lakes and dams is insufficient in the Semiarid Region – National press release</a> \
   </li> \
   <li> \
-  <a href="https://abori.com.br/ambiente/area-de-conservacao-de-entorno-de-lagos-e-represas-precisa-ser-maior-do-que-diz-codigo-florestal/"> \
+  <a href="https://abori.com.br/ambiente/area-de-conservacao-de-entorno-de-lagos-e-represas-precisa-ser-maior-do-que-diz-codigo-florestal/">\
   Conservation area around lakes and dams needs to be larger than what Forest Code states – National press release </a> \
   </li> \
 </ul> \
